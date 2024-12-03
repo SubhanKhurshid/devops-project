1969cd3f3e7732c9244a729667555bf879bfa9c6
SubhanKhurshid <k213096@nu.edu.pk>
2024-12-04 02:09:47 +0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "devops-project"
+AUTHOR_USER_NAME = "SubhanKhurshid"
+SRC_REPO = "secom_failure_detection"
+AUTHOR_EMAIL = "k213096@nu.edu.pk"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    package_data={
+        "": ["*.txt", "*.rst"],
+    },
+)
